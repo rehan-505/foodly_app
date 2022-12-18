0cc3cc0ddb76c81f689eaee54d89da8ef2631d1b
rehan devsinc <hafiz.rehan@devsinc.com>
2022-12-18 12:55:29 +0500

    Initial commit master

---
+++ lib/ui/setup/setup_bottom_sheet_ui.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:foodly_app/app/app.locator.dart';
+import 'package:foodly_app/enums/bottom_sheet_type.dart';
+import 'package:foodly_app/ui/bottom_sheets/notice_sheet/notice_sheet.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+void setupBottomSheetUi() {
+  final bottomSheetService = locator<BottomSheetService>();
+
+  final Map<
+      dynamic,
+      Widget Function(BuildContext, SheetRequest<dynamic>,
+          void Function(SheetResponse<dynamic>))> builders = {
+    BottomSheetType.notice: (context, sheetRequest, completer) =>
+        NoticeSheet(completer: completer, request: sheetRequest),
+  };
+
+  bottomSheetService.setCustomSheetBuilders(builders);
+}
+
